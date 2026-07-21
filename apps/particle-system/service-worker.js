0ac6282c9a4bc4c84754f74d6dc262dd3383cd9b
evs625 <40184274+evs625@users.noreply.github.com>
2026-07-20 23:22:08 -0700

    Cache GPU density engine

--- apps/particle-system/service-worker.js
+++ apps/particle-system/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "particle-system-v3";
+const CACHE_NAME = "particle-system-v4";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -10,6 +10,7 @@ const APP_SHELL = [
   "./icons/icon-maskable.svg",
   "./src/main.js",
   "./src/render/WebGLRenderer.js",
+  "./src/simulation/GpuDensitySimulation.js",
   "./src/simulation/Simulation.js",
   "./src/simulation/SpatialHash.js",
   "./src/simulation/config.js",
@@ -35,7 +36,6 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
-
   event.respondWith(
     caches.match(event.request).then((cached) => {
       const network = fetch(event.request).then((response) => {
@@ -45,7 +45,6 @@ self.addEventListener("fetch", (event) => {
         }
         return response;
       });
-
       return cached || network.catch(() => caches.match("./index.html"));
     }),
   );
